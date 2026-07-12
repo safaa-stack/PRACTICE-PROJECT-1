ddb824e017f70cb70bb255b5f57b5d78f15175a9
safaa <safaashabodien62@gmail.com>
2026-07-13 01:07:16 +0200

    follow
    add changes to index.html
    add changes to script.js

--- script.js
+++ script.js
@@ -12,22 +12,21 @@ document.createElement("li");
     newTask.textContent = 
 taskinput.value;
 
-    const deleteButton =
-    document.createElement("button");
-    deleteButton.textContent =
-"Delete";
-
 const deleteButton =
 document.createElement("button");
-deleteButton.textContent = "Delete";
+    deleteButton.textContent =
+    "Remove"
 
 deleteButton.addEventListener("click" ,
 function () {
+    
     newTask.remove();
-});
-     newTask.appendChild(deleteButton);
 
-     tasklist.appendChild(newTask);
+    });
+    
+    newTask.appendChild(deleteButton);
+    tasklist.appendChild(newTask);
 
-    taskinput.value = "";
-});
+    taskinput.value ="";
+
+}); 
