307e7b32f8ed3ead86dd98b1ea919df170a93a4f
safaa <safaashabodien62@gmail.com>
2026-07-12 16:53:42 +0200

    Update script.js
    update script.js

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const addButton = document.getElementById("addButton");
-const taskinput = document.getElementById("taskinput");
-const tasklist = document.getElementById("tasklist");
+const addButton =
+document.getElementById("addButton");
+const taskinput = 
+document.getElementById("taskinput");
+const tasklist = 
+document.getElementById("tasklist");
 
 addButton.addEventListener("click", function () {
-    const newTask = document.createElement("li");
+    const newTask = 
+    document.createElement("li");
+    newTask.textContent = 
+taskinput.value;
 
-    newTask.textContent = taskinput.value;
+    const deleteButton =
+    document.createElement("button");
+    deleteButton.textContent =
+"Delete";
+ deleteButton.addEvenetListener("click,
+function () {
+        newTask.remove();
+});
+    
+    newTask.appendChild(deleteButton);
 
-    tasklist.appendChild(newTask);
+     tasklist.appendChild(newTask);
 
     taskinput.value = "";
 });
